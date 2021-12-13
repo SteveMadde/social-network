97bfda40f337329667df1e9c1527a1bf7905a71b
Ruslan <russellgonchar@gmail.com>
2021-12-14 02:03:15 +0300

    preloader

---
+++ src/Users/Users.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {UserType} from "../components/Redax/State";
+import userPhoto from "../Users/userPhoto.jpg"
+import s from "./Users.module.css"
+
+
+export type UsersType = {
+    users: UserType[]
+    follow: (userID: number) => void
+    unfollow: (userID: number) => void
+    setUsers: (users: UserType[]) => void
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    setCurrentPage: (page: number) => void
+    setTotalUsersCount: (totalCount: number) => void
+    onPageChanger: (page: number) => void
+}
+
+
+export let Users = (props: UsersType) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+
+    return <div>
+        {pages.map(p => {
+            return <span className={props.currentPage === p ? s.selectedPage : ''}
+                         onClick={(e) => {
+                             props.onPageChanger(p)
+                         }}>{p}</span>
+        })}
+        {props.users.map(user => {
+            return <div>
+                <img src={userPhoto} alt={''}/>
+                {user.id}
+                <div>
+                    {
+                        user.followed ? <button onClick={() => {
+                            props.unfollow(user.id)
+                        }}>
+                            unFollow
+                        </button> : <button onClick={() => {
+                            props.follow(user.id)
+                        }}>Follow</button>
+                    }
+                </div>
+                <div>{user.fullName}</div>
+                <div>{user.status}</div>
+            </div>
+        })}
+    </div>
+}
+
---
+++ src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import preloader from "../../../Users/preloader.svg"
+import React from "react";
+
+
+
+
+export let Preloader = () => {
+    return <div>
+        <img src={preloader} alt=" "/>
+    </div>
+}
